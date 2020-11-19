19d2a04d8ededb12475751b9da5e604de3113d8e
lzamora2798 <lzamora@ad.netpc.ec>
2020-11-19 17:40:30 -0500

    dosage imagen finalizado

--- src/app/medicine/medicine.page.ts
+++ src/app/medicine/medicine.page.ts
@@ -23,6 +23,9 @@ export class MedicinePage implements OnInit {
     observaciones:true,
     citados:true
   }
+
+  private pictogramasArray : any;
+  private dosage_flag :boolean;
   constructor(private activatedRoute: ActivatedRoute,
     private medicineService:MedicineService) { }
 
@@ -30,16 +33,20 @@ export class MedicinePage implements OnInit {
     this.record = this.activatedRoute.snapshot.paramMap.get('record');
     this.medicineService.getDataEachMedicine(this.record).subscribe((res) =>{ //una opcion es enviar el subcribe al service
       this.info =res;
-    
+      this.pictogramasArray = res["picto"].split("|")
+      if (res["dosage_image"].localeCompare("")){
+        this.dosage_flag = true
+      }else{
+        this.dosage_flag = false
+      }
+      //console.log(this.pictogramasArray)
       console.log(this.info)
       //this.databaseService.ResiveArray(this.medicineArrayFinal)
-
     },(error)=>{console.log(error)})
 
   }
 
   clickBotonesInformativos(dato : string){
-    console.log(dato)
     if (!this.banderas[dato]){
       this.banderas[dato]=true
     }else{
